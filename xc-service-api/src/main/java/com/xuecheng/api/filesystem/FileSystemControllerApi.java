dbd3d5ebe3f039bcb4ef2e7b81917179abf60911
lbj <1327614618@qq.com>
2020-01-03 19:58:38 +0800

    图片上传

--- xc-service-api/src/main/java/com/xuecheng/api/filesystem/FileSystemControllerApi.java
+++ xc-service-api/src/main/java/com/xuecheng/api/filesystem/FileSystemControllerApi.java
@@ -1,7 +1,11 @@
 package com.xuecheng.api.filesystem;
 
 import com.xuecheng.framework.domain.filesystem.response.UploadFileResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.multipart.MultipartFile;
+
+@Api(value = "文件上传",description = "文件管理",tags = {"文件上传系统"})
 
 public interface FileSystemControllerApi {
     /**
@@ -12,5 +16,6 @@ public interface FileSystemControllerApi {
      * @param metadata 元信息,json格式
      * @return
      */
+    @ApiOperation("上传文件")
     public UploadFileResult upload(MultipartFile multipartFile, String filetag, String businesskey, String metadata);
 }
--- xc-service-base-filesystem/src/main/java/com/xuecheng/filesystem/service/FileSystemService.java
+++ xc-service-base-filesystem/src/main/java/com/xuecheng/filesystem/service/FileSystemService.java
@@ -1,30 +1,123 @@
 package com.xuecheng.filesystem.service;
 
+import com.alibaba.fastjson.JSON;
 import com.xuecheng.filesystem.dao.FileSystemRepository;
+import com.xuecheng.framework.domain.filesystem.FileSystem;
+import com.xuecheng.framework.domain.filesystem.response.FileSystemCode;
+import com.xuecheng.framework.domain.filesystem.response.UploadFileResult;
+import com.xuecheng.framework.exception.ExceptionCast;
+import com.xuecheng.framework.model.response.CommonCode;
+import org.apache.commons.lang3.StringUtils;
+import org.csource.fastdfs.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.Map;
+
 @Service
 public class FileSystemService {
 
-    @Value("${xuecheng.fastdfs.connect_timeout_in_seconds}")
-    String  connect_timeout_in_seconds;
-    @Value("${xuecheng.fastdfs.network_timeout_in_seconds}")
-    String  network_timeout_in_seconds;
-    @Value("${xuecheng.fastdfs.charset}")
-    String  charset;
+    //fdfs服务器参数
     @Value("${xuecheng.fastdfs.tracker_servers}")
-    String  tracker_servers;
+    String tracker_servers;
+    @Value("${xuecheng.fastdfs.connect_timeout_in_seconds}")
+    int connect_timeout_in_seconds;
+    @Value("${xuecheng.fastdfs.network_timeout_in_seconds}")
+    int network_timeout_in_seconds;
+    @Value("${xuecheng.fastdfs.charset}")
+    String charset;
 
     @Autowired
     FileSystemRepository fileSystemRepository;
 
+    //上传文件
+    public UploadFileResult uploadFileResult(MultipartFile file,
+                                             String filetag,
+                                             String businesskey,
+                                             String metadata) {
+        if (file == null) {
+            ExceptionCast.cast(FileSystemCode.FS_UPLOADFILE_FILEISNULL);
+        }
+        //上传到fdfs
+        String fileId = fdfs_upload(file);
+
+        //创建文件信息对象
+        FileSystem fileSystem = new FileSystem();
+        //文件id
+        fileSystem.setFileId(fileId);
+        //文件在文件系统中的路径
+        fileSystem.setFilePath(fileId);
+        //业务标识
+        fileSystem.setBusinesskey(businesskey);
+        //标签
+        fileSystem.setFiletag(filetag);
+        //元数据
+        if (StringUtils.isNotEmpty(metadata)) {
+            try {
+                Map map = JSON.parseObject(metadata, Map.class);
+                fileSystem.setMetadata(map);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        fileSystem.setFileName(file.getOriginalFilename());
+        //大小
+        fileSystem.setFileSize(file.getSize());
+        //文件类型
+        fileSystem.setFileType(file.getContentType());
+        fileSystemRepository.save(fileSystem);
+        return new UploadFileResult(CommonCode.SUCCESS, fileSystem);
+
+    }
+
     //上传文件到fdfs，返回文件id
-   public String file_upload(MultipartFile file){
-         return null;
-   }
+    public String fdfs_upload(MultipartFile file) {
+        try {
+            //加载fdfs服务器配置
+            initFdfsConfig();
+            //创建追踪者
+            TrackerClient trackerClient = new TrackerClient();
+            //获取追踪者地址
+            TrackerServer trackerServer = trackerClient.getConnection();
+            //获取存储地址
+            StorageServer storeStorage = trackerClient.getStoreStorage(trackerServer);
+            //存储客户端
+            //StorageClient1 storageClient = new StorageClient1(trackerServer, storeStorage);
+            StorageClient1 storageClient1 = new StorageClient1(trackerServer, storeStorage);
+            //上传文件
+            //获取字节
+            byte[] bytes = file.getBytes();
+            //获取文件名
+            String originalFilename = file.getOriginalFilename();
+            //文件扩展名
+            //String extName = originalFilename.substring(originalFilename.lastIndexOf(".") +1);
+            String extName = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
+            //文件id
+            //String file1 = storageClient.upload_file1(bytes, extName, null);
+            String file1 = storageClient1.upload_file1(bytes, extName, null);
+            System.out.println(file1);
+            return file1;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
+    //加载fdfs的配置
+    private void initFdfsConfig() {
+        try {
 
+            ClientGlobal.initByTrackers(tracker_servers);
+            ClientGlobal.setG_connect_timeout(connect_timeout_in_seconds);
+            ClientGlobal.setG_network_timeout(network_timeout_in_seconds);
+            ClientGlobal.setG_charset(charset);
+        } catch (Exception e) {
+            e.printStackTrace();
+            //初始化文件系统失败
+            ExceptionCast.cast(FileSystemCode.FS_INITFDFSERROR);
+        }
+    }
 }
